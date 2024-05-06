50a9d0c654bc10d860d6f03ee83ea47e32e2152e
Nazmul Hasan <shahajahan672@gmail.com>
2024-05-06 19:44:26 +0600

    files added

---
+++ projects/my-library/src/lib/my-library.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-library',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      My first library along with version 1.0.0
+    </p>
+  `,
+  styles: ``
+})
+export class MyLibraryComponent {
+
+}
